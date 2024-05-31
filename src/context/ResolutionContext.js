9bbcda5f28a2a5093b5540f0791b25913cdc8e6f
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2024-05-31 21:30:13 +0200

    resolution array in useState added

--- src/context/ResolutionContext.js
+++ src/context/ResolutionContext.js
@@ -5,30 +5,30 @@ const ResolutionContext = createContext();
 export const ResolutionProvider = ({ children }) => {
   // const [isLoading, setIsLoading] = useState(true);
   const [resolution, setResolution] = useState([
-    // {
-    //   id: 1,
-    //   nickname: "Leo",
-    //   currentStrength: 2,
-    //   goalStrength: 9,
-    //   goal: "I want to bike in 2 months through the alps.",
-    //   themeYear: "I want to be a shining biker!!!",
-    // },
-    // {
-    //   id: 2,
-    //   nickname: "Sue",
-    //   currentStrength: 3,
-    //   goalStrength: 10,
-    //   goal: "I want to win the local dancing championships in September.",
-    //   themeYear: "I want to become a royal dancer !!!.",
-    // },
-    // {
-    //   id: 3,
-    //   nickname: "Andy",
-    //   currentStrength: 4,
-    //   goalStrength: 9,
-    //   goal: "I want to win the next four soccer matches with my team.",
-    //   themeYear: "I want to become a star soccer player!!!",
-    // },
+    {
+      id: 1,
+      nickname: "Leo",
+      currentStrength: 2,
+      goalStrength: 9,
+      goal: "I want to bike in 2 months through the alps.",
+      themeYear: "I want to be a shining biker!!!",
+    },
+    {
+      id: 2,
+      nickname: "Sue",
+      currentStrength: 3,
+      goalStrength: 10,
+      goal: "I want to win the local dancing championships in September.",
+      themeYear: "I want to become a royal dancer !!!.",
+    },
+    {
+      id: 3,
+      nickname: "Andy",
+      currentStrength: 4,
+      goalStrength: 9,
+      goal: "I want to win the next four soccer matches with my team.",
+      themeYear: "I want to become a star soccer player!!!",
+    },
   ]);
   const [resolutionEdit, setResolutionEdit] = useState({
     item: {},
